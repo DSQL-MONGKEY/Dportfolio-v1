b9b7479259632957d09873f407c289feda86995b
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-05 13:09:51 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import About from "@container/About/About"
+
+function App() {
+
+  return (
+    <div className='app'>
+      <About/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/container/About.tsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+   return (
+      <div className="">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt error quibusdam, molestiae, asperiores cumque exercitationem minima eum consequuntur excepturi impedit perspiciatis necessitatibus quas, libero dolorem possimus ea explicabo cum esse.
+      </div>
+   )
+}
+
+export default About
