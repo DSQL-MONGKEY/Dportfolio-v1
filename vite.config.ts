e7f47ef47b76af01f4d1b36f4c41d1588ebb5e0a
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-06 20:00:43 +0700

    build<config>: add import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// also don't forget to `npm i -D @types/node`, so __dirname won't complain
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {find: '@container', replacement: path.resolve(__dirname, 'src/container') },
+      {find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      {find: '@constants', replacement: path.resolve(__dirname, 'src/constants') }
+    ]
+  }
 })
