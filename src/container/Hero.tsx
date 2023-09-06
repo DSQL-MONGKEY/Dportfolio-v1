5c1e6b28b75682d733e46089fb399ebb4b326e28
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-06 19:58:31 +0700

    build<Hero>: initial create

---
+++ src/container/Hero.tsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+  return (
+    <section className="w-full min-h-screen">
+      <div className="flex justify-center items-center">
+         
+      </div>
+    </section>
+  )
+}
+
+export default Hero
