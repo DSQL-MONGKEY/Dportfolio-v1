a3643712a02c8705e42963dfcde032d6645e729e
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-08 23:50:29 +0700

    feat<Skill.tsx>: setup create

--- src/container/Skills.tsx
+++ src/container/Skills.tsx
@@ -1,7 +1,18 @@
+import { skills } from "@constants/constants"
+import { styles } from "@constants/styles"
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className={`${styles.padding} h-screen`}>
+      <div className="flex flex-wrap">
+        {skills.map(skill => (
+          <div className="flex-col" key={skill.title}>
+            <span>{skill.icon}</span>
+            <span>{skill.level}</span>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
