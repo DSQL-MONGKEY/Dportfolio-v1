ef93f29f12497b8dbb6b48ee30c9df4142df1296
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-09 23:05:01 +0700

    feat<Educations>: initial creare

---
+++ src/container/Educations.tsx
@@ -0,0 +1,11 @@
+import { styles } from "@constants/styles"
+
+const Educations = () => {
+   return (
+      <section className={`${styles.padding} ${styles.paddingY} h-screen`}>
+         Educations
+      </section>
+   )
+}
+
+export default Educations
