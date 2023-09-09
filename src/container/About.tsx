9f092d44de35e9352652fcbb2226d24b593154a0
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-09 22:18:42 +0700

    style<About.tsx>: change margin and border

--- src/container/About.tsx
+++ src/container/About.tsx
@@ -7,20 +7,20 @@ const About = () => {
    return (
       <section 
       id="about" 
-      className={`${styles.padding} ${styles.paddingY} w-full h-screen dark:text-white`}>
-         <span className="absolute right-0 text-[20px] sm:text-[30px] mr-10">
+      className={`${styles.padding} ${styles.paddingY} w-full h-[650px] sm:h-[600px] dark:text-white`}>
+         <span className="absolute my-5 right-0 text-[20px] sm:text-[30px] mr-10">
             <GoQuestion/>
          </span>
-         <div className="flex">
-               <Reveal>
-                  <h2
-                  className="font-outfit font-black text-5xl sm:text-7xl">
-                     About
-                  </h2>
-               </Reveal>
-            </div>
-
-         <div className="flex flex-col gap-5 border-l-2  p-5">
+         <div className="flex mt-2">
+            <Reveal>
+               <h2
+               className="font-outfit font-black text-5xl sm:text-6xl">
+                  About
+               </h2>
+            </Reveal>
+         </div>
+         <br />
+         <div className="flex flex-col border-l-2 gap-5 px-5">
             <div className="sm:w-1/2">
                <Reveal>
                   <h2 className="text-2xl font-poppins">
@@ -35,7 +35,7 @@ const About = () => {
             <div className="sm:w-1/2">
                <Reveal>
                   <p className="font-outfit">
-                     As a frontend website and mobile developer. I have intermediate experienced in this field and i hope with this, i can learn something new and deepen the knowledge that i have and also i want what i make, to be useful for many people. 
+                     As a Frontend Developer.I have intermediate experience in this field and i hope that with this, i can learn something new and deepen the knowledge i have and also i want what i create to be useful for many people.
                   </p>
                </Reveal>
             </div>
