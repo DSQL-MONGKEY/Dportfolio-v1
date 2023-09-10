5c76ef5792be464e2c0ea22a1177d1b1c749f38b
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-09-10 10:31:34 +0700

    chore<About>: break commit

--- src/container/Educations.tsx
+++ src/container/Educations.tsx
@@ -1,9 +1,21 @@
+import Reveal from "@components/Reveal"
 import { styles } from "@constants/styles"
+
+import { IoSchoolOutline } from "react-icons/io5"
 
 const Educations = () => {
    return (
       <section className={`${styles.padding} ${styles.paddingY} h-screen`}>
-         Educations
+         <span className="absolute right-0 text-[20px] sm:text-[30px] dark:text-white mr-10 mt-5">
+               <IoSchoolOutline/>
+         </span>
+         <div className="flex">
+            <Reveal>
+               <h3 className="flex font-outfit text-5xl sm:text-6xl font-black dark:text-white">
+                  Educations
+               </h3>
+            </Reveal>
+         </div>
       </section>
    )
 }
