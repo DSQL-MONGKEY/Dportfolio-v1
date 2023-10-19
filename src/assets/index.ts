136543c09bf857367abd547da3e71941bae6fcf5
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-19 18:02:52 +0700

    feat<assets>: add company logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,9 +2,13 @@
 import dlogo from './dlogo.png';
 import dimas from './dimas-photo.png';
 import myCv from './dimas-prasetyo-CV.pdf'
+import agrokimia from './agrokimia.webp'
+import lagundi from './lagundi.png'
 
 export {
    myCv,
    dimas,
    dlogo,
+   agrokimia,
+   lagundi
 };
