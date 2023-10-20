c47be05b139b4ee01f6325d2f9b568c81d7a616f
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-20 21:05:57 +0700

    build<sanity client>: integration api client

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+   readonly VITE_APP_SANITY_PROJECT_ID: string
+   readonly VITE_APP_SANITY_TOKEN: string
+}
+
+interface ImportMeta {
+   readonly env: ImportMetaEnv
+}
