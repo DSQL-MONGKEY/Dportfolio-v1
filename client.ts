590238a9f633b9ddb88cf389361e296d01a11dd7
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-21 10:57:44 +0700

    build<env client>: setup env client

--- client.ts
+++ client.ts
@@ -3,9 +3,9 @@ import imageUrlBuilder from "@sanity/image-url"
 
 
 export const client = createClient({
-   projectId: '',
+   projectId: import.meta.env.VITE_APP_SANITY_PROJECT_ID,
    dataset: 'production',
    useCdn: true,
    apiVersion: '2023-10-20',
-   token: 'skmogMr6dvQ97RYNZYyD2S1WCaoITLk9uODAQt43x25SKfVCEyJKgXp4veaQZ5Fg8I4vzrl5sFFNbfmc2zAVVY9Jimpajq47cFgEQ6OjBeWGcoVYZOeAVuGKVNdJD2jBCGMir3r8jthj4hyrKiV1pi1UXDGDeFQXIzsa1sduQ2q2CLhqqilL'
+   token: import.meta.env.VITE_APP_SANITY_TOKEN
 })
