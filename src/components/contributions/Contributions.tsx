bec365a626af1d73c475840d69f352f394ec9f0d
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-22 09:15:31 +0700

    fix<github acc>: fix the github acc import alias

--- src/components/contributions/Contributions.tsx
+++ src/components/contributions/Contributions.tsx
@@ -1,9 +1,9 @@
 
 import { BsGithub as GithubIcon } from 'react-icons/bs';
 
-import SectionHeading from '@/common/components/elements/SectionHeading';
-import SectionSubHeading from '@/common/components/elements/SectionSubHeading';
-import { GITHUB_ACCOUNTS } from '@/common/constant/github';
+// import SectionHeading from '@/common/components/elements/SectionHeading';
+// import SectionSubHeading from '@/common/components/elements/SectionSubHeading';
+import { GITHUB_ACCOUNTS } from '@constants/constants';
 
 import Calendar from './Calendar';
 import Overview from './Overview';
@@ -18,14 +18,13 @@ export default function Contributions({ githubData }: ContributionsProps) {
       <SectionHeading title="Contributions" icon={<GithubIcon className="mr-1" />} />
       <SectionSubHeading>
         <p className="dark:text-neutral-400">My contributions from last year on github.</p>
-        <Link
+        <a
           href={`https://github.com/${GITHUB_ACCOUNTS.username}`}
           target="_blank"
-          passHref
           className="text-sm font-code text-neutral-600 dark:text-neutral-400 hover:text-neutral-700 hover:dark:text-neutral-400"
         >
           @{GITHUB_ACCOUNTS.username}
-        </Link>
+        </a>
       </SectionSubHeading>
 
       {!githubData && <div className="dark:text-neutral-400">No Data</div>}
