b77102894e8383423bbe4c39e81a81a0d882a3f5
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-22 09:32:08 +0700

    fix<>: comment the missing component

--- src/components/contributions/OverviewItem.tsx
+++ src/components/contributions/OverviewItem.tsx
@@ -1,4 +1,4 @@
-import AnimateCounter from '@/common/components/elements/AnimateCounter';
+// import AnimateCounter from '@/common/components/elements/AnimateCounter';
 
 interface OverviewItemProps {
   label: string;
@@ -11,7 +11,7 @@ export default function OverviewItem({ label, value, unit = '' }: OverviewItemPr
     <div className="flex flex-col self-center rounded-xl bg-neutral-100 py-3 px-4 dark:bg-neutral-800 border border-neutral-200 dark:border-neutral-800">
       <span className="text-sm dark:text-neutral-400">{label}</span>
       <div>
-        <AnimateCounter className="text-xl lg:text-2xl font-medium text-green-600" total={value} />
+        {/* <AnimateCounter className="text-xl lg:text-2xl font-medium text-green-600" total={value} /> */}
         {unit && <span className="text-sm dark:text-neutral-400"> {unit}</span>}
       </div>
     </div>
