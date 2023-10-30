8d6fb5f530b55a91cfe29765f80c8fb25e925b05
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-30 21:05:25 +0700

    feat<toast>: add toast component

--- src/container/Skills.tsx
+++ src/container/Skills.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { GoCodeReview, GoTools, GoFlame } from "react-icons/go"
 
 import Reveal from "@components/Reveal"
@@ -6,8 +6,34 @@ import { techSkills, tools } from "@constants/constants"
 import { styles } from "@constants/styles"
 import Card from "@components/Card"
 
+import { useToast } from "@components/toast/use-toast"
+import { ToastAction } from "@components/toast/toast"
+import { Toaster } from "@components/toast/toaster"
+import { useInView } from "framer-motion"
+
 const Skills = () => {
   const [items, setItems] = useState(techSkills)
+  const ref = useRef(null)
+  const { toast } = useToast()
+  const isInView = useInView(ref, { once: true })
+
+  useEffect(() => {
+    let toastTime:ReturnType<typeof setTimeout>
+    
+    if(isInView) {
+    toastTime = setTimeout(() => (
+        toast({
+          title: "Heii Hoo!",
+          description: "Click the icon to know my knowledge level",
+          action: (
+            <ToastAction altText="Goto">Ok!</ToastAction>
+          )
+        })
+      ), 2000)
+      return () => clearTimeout(toastTime)
+    }
+  }, [isInView, toast])
+
 
   const handleTechButton = () => {
     setItems(techSkills)
@@ -39,19 +65,21 @@ const Skills = () => {
         </Reveal>
       </div>
       
-      <div className="flex flex-col mt-10 justify-center items-center gap-2 ">
+      <div ref={ref} className="flex flex-col mt-10 justify-center items-center gap-2 ">
         <div className="flex gap-5 dark:text-white">
           <button
           onClick={handleTechButton}
           className="flex items-center gap-2 p-2 border-2 font-poppins">
             Tech <GoFlame/>
           </button>
+
           <button 
           onClick={handleToolButton}
           className="flex items-center gap-2 p-2 border-2 font-poppins">
             Tools <GoTools/>
           </button>
         </div>
+        <Toaster />
 
         <Card items={items} />
       </div>
