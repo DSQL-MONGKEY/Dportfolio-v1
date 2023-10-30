eb39ab49bc8853e0b3758512709267a61f62dbeb
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-10-30 21:03:08 +0700

    build<shadcn>: add shadcn component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+export const darkMode = ["class"];
+export const content = [
+  './pages/**/*.{ts,tsx}',
+  './components/**/*.{ts,tsx}',
+  './app/**/*.{ts,tsx}',
+  './src/**/*.{ts,tsx}',
+];
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
+    },
   },
-  plugins: [],
-}
-
+  extend: {
+    keyframes: {
+      "accordion-down": {
+        from: { height: 0 },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: 0 },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+  },
+};
+// eslint-disable-next-line no-undef
+export const plugins = [require("tailwindcss-animate")];
