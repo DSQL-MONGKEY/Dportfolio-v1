745065756189efc27904415159529b1d6e9e34c4
Dimas Prasetyo <[dimasprasetyo7696@gmail.com]>
2023-11-24 08:42:39 +0700

    feat<>: create initiate import for the certificates assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,10 +5,28 @@ import myCv from './dimas-prasetyo-CV.pdf'
 import agrokimia from './agrokimia.webp'
 import lagundi from './lagundi.png'
 
+// Certificates
+import androidBasic from './certificates/Android Basic.webp'
+import backendBeginner from './certificates/Backend Application Beginner.webp'
+import frontendBeginner from './certificates/Frontend Beginner.webp'
+import jsBasic from './certificates/JS Basic.webp'
+import kotlinBasic from './certificates/Kotlin Basic.webp'
+import projectManagement from './certificates/Project Management Basic.webp'
+import reactApp from './certificates/React App.webp'
+
+
+
 export {
    myCv,
    dimas,
    dlogo,
    agrokimia,
-   lagundi
+   lagundi,
+   androidBasic,
+   backendBeginner,
+   frontendBeginner,
+   jsBasic,
+   kotlinBasic,
+   projectManagement,
+   reactApp
 };
