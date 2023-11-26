3ab3cf18862cc0e9b724a58257f3f09e424d6f19
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-11-26 23:46:10 +0700

    feat<>: add certi

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -4,7 +4,7 @@ import { SiMui, SiHtml5, SiXampp, SiIntellijidea, SiAndroidstudio } from "react-
 import { IoSchoolOutline, IoLogoJavascript } from "react-icons/io5"
 import { FaCss3, FaPhp, FaReact, FaBootstrap, FaGitAlt, FaFigma, FaNodeJs } from "react-icons/fa"
 import { TbBrandTypescript, TbBrandTailwind, TbBrandNextjs, TbBrandMysql, TbBrandMongodb, TbBrandVite, TbBrandNotion, TbBrandVscode, TbBrandKotlin, TbBrandGmail } from "react-icons/tb"
-import { agrokimia, lagundi } from "@assets/index"
+import { agrokimia, androidBasic, backendBeginner, bnsp, frontendBeginner, frontendReact, jsBasic, kotlinBasic, lagundi, projectManagement, reactApp } from "@assets/index"
 
 const navLinks = [
    {
@@ -240,14 +240,80 @@ const certificates = [
       title: 'Web Programming',
       institution: 'Badan Nasional Sertifikasi Profesi(BNSP)',
       date: 'Feb - 2022',
-      image: ''
+      image: bnsp,
+      link:''
    },
    {
       title: 'Frontend Developer',
       institution: 'Dicoding ID',
       date: 'August - 2022',
-      image: ''
+      image: frontendBeginner,
+      link: 'https://drive.google.com/file/d/1mOsACSrXC97jDDuY1EioAQp-4ag5AZ1E/view?usp=drive_link'
    },
+   {
+      title: 'Frontend React',
+      institution: 'MySkill',
+      date: 'August - 2022',
+      image: frontendReact,
+      link: 'https://drive.google.com/file/d/12Tn2FmxjCW6NAP3Z8ynF-PEzsRzTT0hw/view?usp=drive_link'
+   },
+   {
+      title: 'React App',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: reactApp,
+      link: 'https://drive.google.com/file/d/1DEEFQr-y_9R1CtpNIGHeUzSRqGYXFKuz/view?usp=drive_link'
+   },
+   {
+      title: 'Android Basic',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: androidBasic,
+      link: 'https://drive.google.com/file/d/1mOsACSrXC97jDDuY1EioAQp-4ag5AZ1E/view?usp=drive_link'
+   },
+   {
+      title: 'Backend Beginner',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: backendBeginner,
+      link: 'https://drive.google.com/file/d/1KAPhr6ZPJbR0UUUUJ42zzN5mu2iidH2n/view?usp=drive_link'
+   },
+   {
+      title: 'Javascript Basic',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: jsBasic,
+      link: 'https://drive.google.com/file/d/1O2viRHkLdVCZvNWXwNXDDA-bKJqnigdc/view?usp=drive_link'
+   },
+   {
+      title: 'Kotlin Basic',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: kotlinBasic,
+      link: 'https://drive.google.com/file/d/14B9te3yWSyK15mGoXnsOXVzd2vb1UvMX/view?usp=drive_link'
+   },
+   {
+      title: 'Project Management Basic',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: projectManagement,
+      link: 'https://drive.google.com/file/d/1Xm3KXui_4e1ylDmpB58coCCuRKsJn4l8/view?usp=drive_link'
+   },
+   {
+      title: 'Structured Query Language (SQL)',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: undefined,
+      link: 'https://drive.google.com/file/d/11IXJFUHt6p5XLdK2Bf7qxyHFae9LYIay/view?usp=drive_link'
+   },
+   {
+      title: 'Software Engineering',
+      institution: 'Dicoding ID',
+      date: 'August - 2022',
+      image: undefined,
+      link: 'https://drive.google.com/file/d/1Pmi4RsDBa3ideoXHcaVXjaiH62HaoUHy/view?usp=drive_link'
+   },
+   
 ]
 
 const experience = [
