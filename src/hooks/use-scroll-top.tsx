9279f2241d062db5bb6aa85e2a64a78ae1ba9ab4
DSQL-MONGKEY <[dimasprasetyo7696@gmail.com]>
2023-11-26 23:47:27 +0700

    feat<>: create responsive scroll navbar hooks

---
+++ src/hooks/use-scroll-top.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export const useScrollTop = (threshold = 10) => {
+   const [scrolled, setScrolled] = useState(false)
+
+   useEffect(() => {
+      const handleScroll = () => {
+         if(window.scrollY > threshold) {
+            setScrolled(true)
+         } else {
+            setScrolled(false)
+         }
+      }
+
+      window.addEventListener("scroll", handleScroll)
+      return () => removeEventListener("scroll",
+      handleScroll)
+   },[threshold]) 
+   return scrolled
+}
