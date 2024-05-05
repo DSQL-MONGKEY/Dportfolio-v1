8226163e2893e5241c753a1a206a12faf56dda7e
Dimas <dimasprasetyo7696@gmail.com>
2024-05-05 22:09:06 +0700

    feat<>: change the contact section into feeds section

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Educations from "@container/Educations"
 import Experiences from "@container/Experiences"
 import Projects from "@container/Projects"
 import Footer from "@components/Footer"
-import Contact from "@container/Contact"
+import Feeds from "@container/Feeds"
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(false)
@@ -22,7 +22,7 @@ function App() {
         <Educations/>
         <Experiences/>
         <Projects/>
-        <Contact/>
+        <Feeds/>
         <Footer/>
       </div>
     </section>
