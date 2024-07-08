1a4e2dac53fa04aa85fe139427c50eb7d37e7b3d
Dimas <dimasprasetyo7696@gmail.com>
2024-07-08 08:33:42 +0700

    feat<>: add theme value into localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,15 @@ import Experiences from "@container/Experiences"
 import Projects from "@container/Projects"
 import Footer from "@components/Footer"
 import Feeds from "@container/Feeds"
+import { cn } from "./lib/utils"
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false) // default Light
+  const localTheme = window.localStorage.getItem('theme')
+  
 
   return (
-    <section className={darkTheme ? 'dark' : ''}>
+    <section className={cn(darkTheme ? 'dark' : localTheme)}>
       <div className="dark:bg-[#252A34] transition-colors duration-300 w-full h-full">
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <Hero/>
